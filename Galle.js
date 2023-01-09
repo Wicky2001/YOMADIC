5ad76da7b304a25157925e85926a6bdca4d8bb03
Wicky <101167965+Wicky2001@users.noreply.github.com>
2023-01-09 17:13:12 +0530

    Add files via upload

---
+++ Galle.js
@@ -0,0 +1,57 @@
+function  newTab1(){
+    console.log("clicked")
+    window.open(
+"galle_destinations_links/LightHouse1.html", "_blank");
+    }
+function  newTab2(){
+    console.log("clicked")
+    window.open(
+"galle_destinations_links/galleFort.html", "_blank");
+    }
+function  newTab3(){
+    console.log("clicked")
+    window.open(
+"galle_destinations_links/jungleBeach.html", "_blank");
+    }
+function  newTab4(){
+    console.log("clicked")
+    window.open(
+"galle_destinations_links/dutchMarket.html", "_blank");
+    }
+function  newTab5(){
+    console.log("clicked")
+    window.open(
+"galle_destinations_links/turtleHatchery.html", "_blank");
+    }
+function  newTab6(){
+    console.log("clicked")
+    window.open(
+"galle_destinations_links/mariTime_museum.html", "_blank");
+    }
+function  newTab7(){
+    console.log("clicked")
+    window.open(
+"galle_destinations_links/singharaja.html", "_blank");
+    }
+function  newTab8(){
+    console.log("clicked")
+    window.open(
+"galle_destinations_links/unawatuna.html", "_blank");
+    }
+let q1=document.querySelector(".more-galle-q")
+console.log(q1)
+
+function paradis(){
+    q1.style.height="auto";
+    console.log("clicked");
+
+}
+
+// q1.addEventListener(onclick,function(){
+
+//     let paraEl=document.querySelector(".more-galle-para")
+    
+
+
+// })
+
---
+++ README.md
@@ -0,0 +1,9 @@
+# YOMADIC BLOG SITE
+
+## Team members
+
+CS/2020/035 - P.M.Wickramarathna ==> Home page & Kandy blog page
+CS/2020/037 - HMBS Induwara ==> Galle blog page
+CS/2020/029 - V.M.Athukorala ==> Jaffna blog page
+CS/2020/001 - R.K.W.Gunathilaka ==> Matara blog page
+CS/2020/028 - C.M.P.S.Chandrasekara ==> Badulla blog page
